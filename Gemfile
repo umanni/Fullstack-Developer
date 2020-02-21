e41b9a87645ffb6be819f6aecdbf0a5a908315d3
Cassiano Blonski Sampaio <cassiano.sampaio@codeminer42.com>
2020-02-21 17:35:42 -0300

    Add User views and controller

--- Gemfile
+++ Gemfile
@@ -11,15 +11,15 @@ gem 'activeadmin', '~> 2.6', '>= 2.6.1'
 gem 'activeadmin_addons', '~> 1.7', '>= 1.7.1'
 gem 'arctic_admin', '~> 3.1', '>= 3.1.1'
 gem 'bootsnap', '>= 1.4.2', require: false
+gem 'bootstrap', '~> 4.4', '>= 4.4.1'
 gem 'devise', '~> 4.7', '>= 4.7.1'
 gem 'jbuilder', '~> 2.7'
+gem 'jquery-rails'
 gem 'pg', '>= 0.18', '< 2.0'
 gem 'puma', '~> 4.1'
 gem 'sass-rails', '>= 6'
 gem 'turbolinks', '~> 5'
 gem 'webpacker', '~> 4.0'
-gem 'bootstrap', '~> 4.4', '>= 4.4.1'
-gem 'jquery-rails'
 
 # Downgraded sprockets because of a bug with sprocket-4.0
 # https://tosbourn.com/fixing-an-issue-with-sprockets-and-feature-tests/
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,24 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  devise_for :users, :path_names => { sign_up: "register" }
+
+  get 'profile', to: 'users#show'
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'logout', to: 'devise/sessions#destroy'
+    delete 'logout', to: 'devise/sessions#destroy'
+  end
+
+  authenticated :user do
+    root to: 'users#show', as: :authenticated_user
+  end
+
+  unauthenticated :user do
+    devise_scope :user do
+      root to: 'devise/sessions#new'
+    end
+  end
 end
