ceb87970841629fe5840db5015e60482f9b5f551
Tiago Leal <tiago.sleal12@gmail.com>
2020-07-10 20:47:06 -0300

    Create initial setup

---
+++ Api/app/controllers/api/v1/api_controller.rb
@@ -0,0 +1,6 @@
+module Api
+  module V1
+    class ApiController < ApplicationController
+      #metodos globais da versão 1
+  end
+end
---
+++ Api/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Instala as Gems
+bundle check || bundle install
+ 
+# Roda nosso servidor
+bundle exec puma -C config/puma.rb
