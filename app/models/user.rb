4b098b8f524c87e91d2595ddc0545c758cfa8b9e
Raí Gondim <raicg2@gmail.com>
2020-07-20 02:44:55 -0300

    updating user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
 
   before_validation :attach_avatar_from_url!
   validates :full_name, presence: true
-  validates :avatar, attached: true, content_type: ['image/jpg', 'image/jpeg', 'image/png'], size: { less_than: 10.megabytes }
+  validates :avatar, content_type: ['image/jpg', 'image/jpeg', 'image/png'], size: { less_than: 10.megabytes }
 
   private
     def attach_avatar_from_url!
