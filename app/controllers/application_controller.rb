cc72a9ce32f14f1bf1f20e34ef5fc7ceee8c2cff
Raí Gondim <raicg2@gmail.com>
2020-07-20 02:41:22 -0300

    configuring application to use login and application layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,21 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  layout :layout_by_resource
+
   def after_sign_in_path_for(resource)
     if resource.admin?
       admin_dashboard_index_path
     else
       user_profile_path
+    end
+  end
+
+  def layout_by_resource
+    if devise_controller? && !request.fullpath.match?('/users/edit') && !(request.fullpath.match('/users') && params[:action] == 'update')
+      'login'
+    else
+      'application'
     end
   end
 
