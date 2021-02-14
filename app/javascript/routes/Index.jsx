d6e7838dd6a366bb277c4e69e6e898d6c2a561f5
Marcelo <santosmarcelo1995@live.com>
2021-02-14 14:58:12 -0300

    Add profile to router

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from '../components/Home';
 import SignIn from '../components/SignIn';
 import SignUp from '../components/SignUp';
+import Profile from '../components/Profile';
 
 export default (
   <Router>
@@ -10,6 +11,7 @@ export default (
       <Route path="/" exact component={Home} />
       <Route path="/sign_in" exact component={SignIn} />
       <Route path="/sign_up" exact component={SignUp} />
+      <Route path="/profile" exact component={Profile} />
     </Switch>
   </Router>
 );
