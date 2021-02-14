4f76d8c6ac2d16bf332764f3843d4c421fc261cc
Marcelo <santosmarcelo1995@live.com>
2021-02-14 13:51:34 -0300

    Permit the other fields for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:full_name, :avatar_image, :role, :email, :password) }
+      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:full_name, :avatar_image, :role, :email, :password, :current_password) }
+    end
 end
