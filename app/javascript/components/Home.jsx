388ec928dc7b9d58e528ef0c1bc2c6fe951f8b16
Marcelo <santosmarcelo1995@live.com>
2021-02-14 14:00:48 -0300

    Add styles to home

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Button } from '@material-ui/core';
+import { Container, Button, makeStyles } from '@material-ui/core';
 
-export default () => (
-  <Container maxWidth="sm">
-    <h1 className="display-4">Home</h1>
-    <hr className="my-4" />
-    <Link to="/sign_in" role="button">
-      <Button variant="contained" color="primary">
-        Sign In
-      </Button>
-    </Link>
-    <Link to="/sign_up" role="button">
-      <Button variant="contained" color="secondary">
-        Sign Up
-      </Button>
-    </Link>
-  </Container>
-);
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& .MuiButton-root': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+export default () => {
+  const classes = useStyles();
+
+  return (
+    <Container maxWidth="sm" className={classes.root}>
+      <h1 className="display-4">Home</h1>
+      <hr className="my-4" />
+      <Link to="/sign_in" role="button">
+        <Button variant="contained" color="primary">
+          Sign In
+        </Button>
+      </Link>
+      <Link to="/sign_up" role="button">
+        <Button variant="contained" color="secondary">
+          Sign Up
+        </Button>
+      </Link>
+    </Container>
+  );
+};
