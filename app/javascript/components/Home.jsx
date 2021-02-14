c24d5d244d0553635dde8146d4dc13cdc22e0aad
Marcelo <santosmarcelo1995@live.com>
2021-02-14 10:55:50 -0300

    Eslint save

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,23 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Button, TextField } from '@material-ui/core';
 
 export default () => (
-  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
-    <div className="jumbotron jumbotron-fluid bg-transparent">
-      <div className="container secondary-color">
-        <h1 className="display-4">Home</h1>
-        <p className="lead">
-          Home
-        </p>
-        <hr className="my-4" />
-        <Link
-          to="/sign_in"
-          className="btn btn-lg custom-button"
-          role="button"
-        >
-          Sign in
-        </Link>
-      </div>
-    </div>
-  </div>
+  <Container maxWidth="sm">
+    <h1 className="display-4">Home</h1>
+    <p className="lead">Home</p>
+    <hr className="my-4" />
+    <Link to="/sign_in" className="btn btn-lg custom-button" role="button">
+      Sign in
+    </Link>
+  </Container>
 );
--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "../components/Home";
-import SignIn from "../components/SignIn";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from '../components/Home';
+import SignIn from '../components/SignIn';
 
 export default (
   <Router>
