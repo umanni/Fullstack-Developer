1cac9a027ba306755922c2a3e7de8c02856a83fb
Marcelo <santosmarcelo1995@live.com>
2021-02-14 14:50:29 -0300

    Add inherited devise sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'homepage#index'
-  
-  devise_for :users, path: '/api/users'
-  
+
+  devise_for :users, :controllers => { :sessions => 'users/sessions'}, path: '/api/users'
+
   match '*path', to: 'homepage#index', via: :all
 end
