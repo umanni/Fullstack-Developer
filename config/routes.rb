cda2e40dd6f205f8e30a28a5d9e4e20fc3b939e5
Marcelo <santosmarcelo1995@live.com>
2021-02-15 10:02:36 -0300

    Add inherited users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'homepage#index'
 
-  devise_for :users, :controllers => { :sessions => 'users/sessions'}, path: '/api/users'
+  devise_for :users, :controllers => { :sessions => 'users/sessions', :registrations => 'users'}, path: '/api/users'
+  devise_scope :user do
+    get '/api/users/:id' => 'users#edit', :as => 'user_show'
+  end
 
-  match '*path', to: 'homepage#index', via: :all
+  match '*path', to: 'homepage#index', via: :all, :as => 'react_base'
 end
