397692d2b428ddf037d7d5bcf265d3ec8dd01ec6
Marcelo <santosmarcelo1995@live.com>
2021-02-15 23:09:12 -0300

    Update react routes files

--- app/javascript/routes/Index.js
+++ app/javascript/routes/Index.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { AuthProvider } from '../hooks/Auth';
-import Home from '../components/Home';
-import SignIn from '../components/SignIn';
-import SignUp from '../components/SignUp';
-import Profile from '../components/Profile';
+import MainProvider from '@/contexts/MainContext';
+import Profile from '@/components/Profile';
 
 export default (
   <Router>
-    <AuthProvider>
+    <MainProvider>
       <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/sign_in" exact component={SignIn} />
-        <Route path="/sign_up" exact component={SignUp} />
-        <Route path="/profile/:id" exact component={Profile} />
-        <Route path="/edit/:id" exact component={SignUp} />
+        <Route path="/" exact component={Profile} />
       </Switch>
-    </AuthProvider>
+    </MainProvider>
   </Router>
 );
