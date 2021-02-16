4b85e097a34e0f411d70f9dc4d59798f914d2d2f
Marcelo <santosmarcelo1995@live.com>
2021-02-16 11:51:27 -0300

    Update react routes

--- app/javascript/routes/Index.js
+++ app/javascript/routes/Index.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+
+import Route from '@/routes/Route';
+
 import MainProvider from '@/contexts/MainContext';
 import Profile from '@/components/Profile';
+import Dashboard from '@/components/Dashboard';
 
 export default (
   <Router>
     <MainProvider>
       <Switch>
-        <Route path="/" exact component={Profile} />
+        <Route path="/profile" exact isPrivate component={Profile} />
+        <Route path="/dashboard" exact isPrivate isAdmin component={Dashboard} />
       </Switch>
     </MainProvider>
   </Router>
