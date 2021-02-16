8d12acf8b2a421e8fbda84756fe65fd5cb90344d
Marcelo <santosmarcelo1995@live.com>
2021-02-16 18:43:00 -0300

    Dashboard and simple card update

---
+++ app/javascript/components/DashboardSimpleCard.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import {
+  Avatar,
+  Box,
+  Card,
+  CardContent,
+  Grid,
+  Typography,
+  colors,
+  makeStyles
+} from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100%',
+    width: 512,
+  },
+  avatar: {
+    backgroundColor: colors.green[600],
+    height: 56,
+    width: 56
+  },
+  differenceIcon: {
+    color: colors.green[900]
+  },
+  differenceValue: {
+    color: colors.green[900],
+    marginRight: theme.spacing(1)
+  }
+}));
+
+const TotalCustomers = ({headerText, cardValue, customIcon, className, ...rest }) => {
+  const classes = useStyles();
+
+  return (
+    <Card
+      className={clsx(classes.root, className)}
+      {...rest}
+    >
+      <CardContent>
+        <Grid
+          container
+          justify="space-between"
+          spacing={3}
+        >
+          <Grid item>
+            <Typography
+              color="textSecondary"
+              gutterBottom
+              variant="h6"
+            >
+              { headerText }
+            </Typography>
+            <Typography
+              color="textPrimary"
+              variant="h3"
+            >
+              { cardValue }
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Avatar className={classes.avatar}>
+              { customIcon }
+            </Avatar>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
+
+TotalCustomers.propTypes = {
+  className: PropTypes.string
+};
+
+export default TotalCustomers;
--- app/javascript/pages/Dashboard.js
+++ app/javascript/pages/Dashboard.js
@@ -1,12 +1,51 @@
 import React from 'react';
+import {
+  Container,
+  Grid,
+  makeStyles,
+  Box
+} from '@material-ui/core';
+import PeopleIcon from '@material-ui/icons/PeopleOutlined';
+import WarningIcon from '@material-ui/icons/WarningOutlined';
 
 import TopBar from '@/components/TopBar'
+import DashboardSimpleCard from '@/components/DashboardSimpleCard'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.dark,
+    minHeight: '100%',
+    paddingBottom: theme.spacing(3),
+    paddingTop: 72,
+  }
+}));
 
 export default () => {
+  const classes = useStyles();
+
   return (
     <>
       <TopBar></TopBar>
-      <div>AQUIs</div>
+      <Box className={classes.root}>
+        <Container maxWidth={false}>
+          <Grid container spacing={3}>
+            <Grid item>
+              <DashboardSimpleCard
+                headerText="Users:"
+                cardValue="10"
+                customIcon={<PeopleIcon />}
+              />
+            </Grid>
+            <Grid item>
+              <DashboardSimpleCard
+                headerText="Administrators:"
+                cardValue="5"
+                customIcon={<WarningIcon />}
+              />
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
     </>
   );
 };
