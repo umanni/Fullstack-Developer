f334fe1a5f38e95f0d4caf688c29cbf8a653dd44
Marcelo <santosmarcelo1995@live.com>
2021-02-15 23:10:25 -0300

    Update devise components

--- app/javascript/components/SignIn.js
+++ app/javascript/components/SignIn.js
@@ -1,8 +1,6 @@
-import React, { useCallback } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import React from 'react';
+import { csrfToken } from 'rails-ujs'
 import { Container, Button, TextField, makeStyles } from '@material-ui/core';
-
-import { useAuth } from '../hooks/Auth';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -18,36 +16,15 @@ const useStyles = makeStyles(theme => ({
 
 export default () => {
   const classes = useStyles();
-  const { signIn } = useAuth();
-  const history = useHistory();
-
-  const handleSubmit = useCallback(async event => {
-    event.preventDefault();
-
-    const { email, password } = event.target;
-
-    await signIn({
-      email: email.value,
-      password: password.value,
-    });
-
-    const id = JSON.parse(localStorage.getItem('@FullstackDeveloper:id'));
-    const admin = JSON.parse(localStorage.getItem('@FullstackDeveloper:admin'));
-
-    if (id) {
-      if (!admin) {
-        history.push(`/profile/${id}`);
-      }
-    }
-  }, []);
 
   return (
     <Container maxWidth="sm">
       <h1 className="display-4 text-center">Sign In</h1>
-      <form className={classes.root} onSubmit={handleSubmit}>
-        <TextField id="email" variant="outlined" label="Email" />
+      <form action="/users/sign_in" method="post" className={classes.root}>
+        <input type="hidden" name="authenticity_token" value={csrfToken()}/>
+        <TextField name="user[email]" variant="outlined" label="Email" />
         <TextField
-          id="password"
+          name="user[password]"
           variant="outlined"
           label="Password"
           type="password"
@@ -56,11 +33,6 @@ export default () => {
         <Button type="submit" variant="contained" color="secondary">
           Login
         </Button>
-        <Link to="/" role="button">
-          <Button variant="contained" color="primary">
-            Home
-          </Button>
-        </Link>
       </form>
     </Container>
   );
