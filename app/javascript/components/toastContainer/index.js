5eaf91a6aa007dc3bdf7da7afed1eeeca04575d7
Marcelo <santosmarcelo1995@live.com>
2021-02-17 14:34:21 -0300

    Add toast messages

---
+++ app/javascript/components/toastContainer/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useTransition } from 'react-spring';
+
+import Toast from './toast';
+
+import { Container } from './styles';
+
+import { ToastMessage } from '@/hooks/Toast';
+
+export default ({ messages }) => {
+  const messagesWithTransitions = useTransition(
+    messages,
+    message => message.id,
+    {
+      from: { right: '-120%', opacity: 0 },
+      enter: { right: '0%', opacity: 1 },
+      leave: { right: '-120%', opacity: 0 },
+    },
+  );
+
+  return (
+    <Container>
+      {messagesWithTransitions.map(({ item, key, props }) => {
+        return <Toast key={key} message={item} style={props} />;
+      })}
+    </Container>
+  );
+};
