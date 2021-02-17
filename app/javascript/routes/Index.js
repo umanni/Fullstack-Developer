881614a1a8f8ad74a03ef2b4600445430c5b77b3
Marcelo <santosmarcelo1995@live.com>
2021-02-17 14:35:02 -0300

    Add toast as provider

--- app/javascript/routes/Index.js
+++ app/javascript/routes/Index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 
 import Route from '@/routes/Route';
+import { ToastProvider } from '@/hooks/Toast';
 
 import MainProvider from '@/contexts/MainContext';
 import Profile from '@/pages/Profile';
@@ -12,13 +13,15 @@ import Import from '@/pages/import';
 export default (
   <Router>
     <MainProvider>
-      <Switch>
-        <Route path="/" exact component={Profile} />
-        <Route path="/profile" exact isPrivate component={Profile} />
-        <Route path="/edit/:id" exact isPrivate component={EditUser} />
-        <Route path="/dashboard" exact isPrivate isAdmin component={Dashboard} />
-        <Route path="/import" exact isPrivate isAdmin component={Import} />
-      </Switch>
+      <ToastProvider>
+        <Switch>
+          <Route path="/" exact component={Profile} />
+          <Route path="/profile" exact isPrivate component={Profile} />
+          <Route path="/edit/:id" exact isPrivate component={EditUser} />
+          <Route path="/dashboard" exact isPrivate isAdmin component={Dashboard} />
+          <Route path="/import" exact isPrivate isAdmin component={Import} />
+        </Switch>
+      </ToastProvider>
     </MainProvider>
   </Router>
 );
