3f2567034a94a17fa42fe1aaa524f8a714a7e9ab
Marcelo <santosmarcelo1995@live.com>
2021-02-20 12:36:53 -0300

    Adjust editUser page

--- app/javascript/pages/EditUser.js
+++ app/javascript/pages/EditUser.js
@@ -5,7 +5,7 @@ import React, {
   useState,
   useContext,
 } from 'react';
-import { Link, useRouteMatch, useHistory } from 'react-router-dom';
+import { useRouteMatch, useHistory } from 'react-router-dom';
 import {
   Container,
   Button,
@@ -15,12 +15,14 @@ import {
   Switch,
   Box,
 } from '@material-ui/core';
-import { positions } from '@material-ui/system';
+import * as Yup from 'yup';
 
-import { MainContext } from '@/contexts/MainContext';
+import { MainContext, setMyPosition } from '@/contexts/MainContext';
 import { useToast } from '@/hooks/Toast';
+import getValidationErrors from '@/utils/getValidationErrors';
 
 import TopBar from '@/components/TopBar';
+import SideBar from '@/components/sidebar/SideBar';
 
 import Api from '@/services/api';
 
@@ -38,17 +40,26 @@ const useStyles = makeStyles(theme => ({
 
 export default () => {
   const classes = useStyles();
-  const { currentUser, updateValue } = useContext(MainContext);
+  const { currentUser, updateValue, setMyPosition } = useContext(MainContext);
   const { params } = useRouteMatch();
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState({});
   const [inputState, setInputState] = useState({
     full_name: '',
     email: '',
     avatar_image: '',
     admin: false,
   });
+  const [formError, setFormError] = useState({
+    full_name: '',
+    email: '',
+    avatar_image: '',
+  })
   const {addToast} = useToast();
   const history = useHistory();
+
+  useEffect(() => {
+    setMyPosition('Edit user');
+  }, []);
 
   useEffect(() => {
     const { id } = params;
@@ -61,14 +72,12 @@ export default () => {
   }, [params]);
 
   useEffect(() => {
-    if (user) {
-      setInputState({
-        full_name: user.full_name,
-        email: user.email,
-        avatar_image: user.avatar_image,
-        admin: user.admin,
-      });
-    }
+    setInputState({
+      full_name: user.full_name,
+      email: user.email,
+      avatar_image: user.avatar_image,
+      admin: user.admin,
+    });
   }, [user]);
 
   const inputChange = useCallback(
@@ -97,72 +106,106 @@ export default () => {
         admin,
       } = event.target;
 
-      Api.put(`/api/users/${id}`, {
-        user: {
+      try {
+        const schema = Yup.object().shape({
+          full_name: Yup.string().required('The full name is required'),
+          email: Yup.string().required('Email required').email('Insert a valid email'),
+          avatar_image: Yup.string().required('Insert the image URL path'),
+        })
+
+        await schema.validate({
           full_name: full_name.value,
           email: email.value,
-          avatar_image: avatar_image.value,
-          admin: admin.checked,
-        },
-      }).then(response => {
-        const {id, full_name, email, avatar_image, admin} = response.data;
+          avatar_image: avatar_image.value
+        }, {abortEarly: false});
 
-        if (currentUser.id == id) {
-          updateValue({
-            id,
-            full_name,
-            email,
-            avatar_image,
-            admin
+        Api.put(`/api/users/${id}`, {
+          user: {
+            full_name: full_name.value,
+            email: email.value,
+            avatar_image: avatar_image.value,
+            admin: admin.checked,
+          },
+        }).then(response => {
+          const {id, full_name, email, avatar_image, admin} = response.data;
+
+          if (currentUser.id == id) {
+            updateValue({
+              id,
+              full_name,
+              email,
+              avatar_image,
+              admin
+            });
+          }
+          addToast({
+            type: 'success',
+            title: 'Success!',
+            description:
+              'User updated successfuly!',
           });
-        }
-        addToast({
-          type: 'success',
-          title: 'Success!',
-          description:
-            'User updated successfuly!',
-        });
 
-        history.push('/');
-      });
+          history.push('/');
+        });
+      } catch(error) {
+        if (error instanceof Yup.ValidationError) {
+          const serializedErrors = getValidationErrors(error);
+          setFormError(serializedErrors);
+
+          return;
+        }
+      }
     },
     [params],
   );
 
+  const handleSelfDelete = useCallback(() => {
+    Api.delete(`/api/users/${currentUser.id}`)
+    .then(() => {
+      location.reload();
+    });
+  }, []);
+
   return (
     <>
       <TopBar />
-      <Container maxWidth="sm">
-        <h1 className="display-4 text-center">Edit {user && user.full_name} profile</h1>
+      {currentUser.admin && <SideBar />}
+      <Container maxWidth="sm" style={{marginTop: '72px'}}>
         <form className={classes.root} onSubmit={handleSubmit}>
           <TextField
             id="full_name"
+            error={!!formError.full_name}
+            helperText={formError.full_name}
             variant="outlined"
             label="Full name"
-            value={inputState.full_name}
+            value={inputState.full_name ? inputState.full_name : ''}
             onChange={inputChange}
           />
           <TextField
             id="email"
+            error={!!formError.email}
+            helperText={formError.email}
             variant="outlined"
             label="Email"
-            value={inputState.email}
+            value={inputState.email ? inputState.email : ''}
             onChange={inputChange}
           />
           <TextField
             id="avatar_image"
+            error={!!formError.avatar_image}
+            helperText={formError.avatar_image}
             variant="outlined"
             label="Avatar url"
-            value={inputState.avatar_image}
+            value={inputState.avatar_image ? inputState.avatar_image : ''}
             onChange={inputChange}
           />
           {currentUser.admin
             ? (
-              <Box flexGrow={1} left="60%" position="absolute">
+              <Box flexGrow={1} left="57%" position="absolute">
                 <FormControlLabel
                   control={
                     <Switch
-                      checked={inputState.admin}
+                      checked={inputState.admin ? inputState.admin : false}
                       onChange={switchChange}
                       id="admin"
                       color="primary"
@@ -176,14 +219,12 @@ export default () => {
           }
           <br/><br/><br/>
           <hr className="my-4" />
-          <Button type="submit" variant="contained" color="secondary">
+          <Button type="submit" variant="contained" color="primary">
             Update user
           </Button>
-          <Link to="/" role="button">
-            <Button variant="contained" color="primary">
-              Home
-            </Button>
-          </Link>
+          <Button variant="contained" color="secondary" onClick={handleSelfDelete}>
+            Delete Account
+          </Button>
         </form>
       </Container>
     </>
