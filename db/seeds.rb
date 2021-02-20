2f95593eaa0b81fa0b347fca76914b4eaa062e15
Marcelo <santosmarcelo1995@live.com>
2021-02-20 12:57:49 -0300

    Add admin seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create({
+  full_name: 'admin',
+  email: 'admin@admin.com',
+  password: '123456',
+  password_confirmation: '123456',
+  avatar_image: 'https://images.vexels.com/media/users/3/145908/preview2/52eabf633ca6414e60a7677b0b917d92-criador-de-avatar-masculino.jpg',
+  admin: true
+})
