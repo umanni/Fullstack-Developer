a2b0cebcb9dfbe8caaf6e03adb727d24c8f6def5
Wander Rodrigues <34662101+WanderRodriguesUmanni@users.noreply.github.com>
2022-02-14 09:36:37 -0400

    fix(add): redirect loged user to users

--- app/controllers/site/welcome_controller.rb
+++ app/controllers/site/welcome_controller.rb
@@ -1,4 +1,12 @@
 class Site::WelcomeController < SiteController
+  before_action :redirect_back
+
+  def redirect_back
+    if user_signed_in?
+      redirect_to users_backoffice_admin_index_path
+    end
+  end
+
   def index
   end
 end
