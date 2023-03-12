4a41ab7f5d98de622026fa71d56acf4e88090f4f
Rafael de Paula <xspynks@gmail.com>
2023-03-12 17:21:59 -0300

    Add custom controllers for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
+    # Define custom controllers for devise
     registrations: 'users/registrations',
     sessions: 'users/sessions',
   }
-  
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    if Rails.env.development?
+      # Create a get route for sign out on devise sessions controller in development mode
+      get "users/sign_out" => "users/sessions#destroy", via: :delete
+    end
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
