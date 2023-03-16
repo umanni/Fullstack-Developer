52ca8403c6fd54da42c2d7ec64ee77e8470bfd56
Rafael de Paula <xspynks@gmail.com>
2023-03-16 00:04:09 -0300

    fix: delete unused migrations

--- db/migrate/20230315042416_add_user_ref_to_user_profile.rb
+++
@@ -1,5 +0,0 @@
-class AddUserRefToUserProfile < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :user_profiles, :user, null: false, foreign_key: true
-  end
-end
--- db/migrate/20230316002434_remove_user_profile.rb
+++
@@ -1,6 +0,0 @@
-class RemoveUserProfile < ActiveRecord::Migration[7.1]
-  def change
-     # remove the entire UserProfile table
-    drop_table :user_profiles
-  end
-end
