644f95b2828c6db3049476516595079088ced471
Pedro Henrique Batista Costa <pedrobc.engsoft@gmail.com>
2023-05-22 21:07:35 -0300

    update user model

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -3,12 +3,14 @@ class User < ActiveRecord::Base
           :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 
-  include DeviseTokenAuth::Concerns::User
-
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :image, presence: true
+  # validates :image, presence: true
   validates :profile, presence: true
 
   enum profile: { admin: 0, client: 1 }
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
