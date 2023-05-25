f0d4f9c038c152ff29aa29804d07965e57936674
Pedro Henrique Batista Costa <pedrobc.engsoft@gmail.com>
2023-05-25 11:06:25 -0300

    update routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,18 +2,17 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
-  Navigate,
 } from 'react-router-dom';
-import { UserProvider, useUser } from './hooks/useUser';
+import { UserProvider } from './hooks/useUser';
 import ProfilePage from './pages/Auth/ProfilePage';
 import { LoggedAdmin } from './pages/Auth/LoggedAdmin';
 import Home from './pages/Home';
 import RegisterPage from './components/Register';
 import 'antd/dist/reset.css';
-import './App.scss';
+// import './App.scss';
 
 function App() {
-  const { currentUser } = useUser();
+  
 
   return (
     <UserProvider>
@@ -22,9 +21,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/user/profile" element={<ProfilePage />} />
-          {/* {!currentUser && (
-            <Route path="/user/profile" element={<Navigate to="/" replace />} />
-          )} */}
           <Route path="/admin/users/" element={<LoggedAdmin />} />
         </Routes>
       </Router>
