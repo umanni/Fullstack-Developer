08c0eb132ce93961f405bc156b816408caf3653d
Pedro Henrique Batista Costa <pedrobc.engsoft@gmail.com>
2023-05-25 11:13:49 -0300

    update user model

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -5,7 +5,6 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  # validates :image, presence: true
   validates :profile, presence: true
 
   enum profile: { admin: 0, client: 1 }
