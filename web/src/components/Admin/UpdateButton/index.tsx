9cabff426988e105b2bac8aec2cbc21c4dd86f4f
Pedro Henrique Batista Costa <pedrobc.engsoft@gmail.com>
2023-05-26 21:48:50 -0300

    update admin update component

--- web/src/components/Admin/UpdateButton/index.tsx
+++ web/src/components/Admin/UpdateButton/index.tsx
@@ -1,24 +1,25 @@
-import { Button, Modal, Input, Select } from 'antd';
+import { Button, Modal, Form, Input, Select } from 'antd';
 import { useState } from 'react';
 import { useUser } from '../../../hooks/useUser';
 import { updateUser } from '../../../lib/api';
 import { DataType } from '../../../utils/DataType';
+import { EditTwoTone } from '@ant-design/icons';
+import './styles.scss';
 
 const { Option } = Select;
 
 const UpdateButton: React.FC<{ record: DataType }> = ({ record }) => {
   const { setCurrentUser } = useUser();
+  const [form] = Form.useForm();
   const [modalVisible, setModalVisible] = useState(false);
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [profile, setProfile] = useState('');
 
   const openModal = () => {
-    setFirstName(record.first_name);
-    setLastName(record.last_name);
-    setEmail(record.email);
-    setProfile(record.profile);
+    form.setFieldsValue({
+      firstName: record.first_name,
+      lastName: record.last_name,
+      email: record.email,
+      profile: record.profile,
+    });
     setModalVisible(true);
   };
 
@@ -26,13 +27,13 @@ const UpdateButton: React.FC<{ record: DataType }> = ({ record }) => {
     setModalVisible(false);
   };
 
-  const handleUpdate = async () => {
+  const handleUpdate = async (values: any) => {
     try {
       const updatedUser = await updateUser(record.id, {
-        first_name: firstName,
-        last_name: lastName,
-        email: email,
-        profile: profile,
+        first_name: values.firstName,
+        last_name: values.lastName,
+        email: values.email,
+        profile: values.profile,
       });
 
       if (setCurrentUser) {
@@ -47,7 +48,9 @@ const UpdateButton: React.FC<{ record: DataType }> = ({ record }) => {
 
   return (
     <>
-      <Button onClick={openModal}>Update</Button>
+      <Button onClick={openModal}>
+        <EditTwoTone />
+      </Button>
       <Modal
         title="Update User"
         open={modalVisible}
@@ -56,34 +59,60 @@ const UpdateButton: React.FC<{ record: DataType }> = ({ record }) => {
           <Button key="cancel" onClick={closeModal}>
             Cancel
           </Button>,
-          <Button key="action" type="primary" onClick={handleUpdate}>
+          <Button key="action" type="primary" onClick={() => form.submit()}>
             Update
           </Button>,
         ]}
       >
-        <Input
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-          placeholder="First Name"
-        />
-        <Input
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-          placeholder="Last Name"
-        />
-        <Input
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-        />
-        <Select
-          value={profile}
-          onChange={setProfile}
-          placeholder="Select Profile"
-        >
-          <Option value="admin">Admin</Option>
-          <Option value="client">Client</Option>
-        </Select>
+        <Form className="updateModal" form={form} onFinish={handleUpdate}>
+          <Form.Item
+            name="firstName"
+            rules={[
+              // { required: true, message: 'Please input your first name!' },
+              {
+                pattern: /^[A-Za-z]+$/,
+                message: 'Please input alphabets only!',
+              },
+            ]}
+          >
+            <Input className="firstNameInput" placeholder="First Name" />
+          </Form.Item>
+
+          <Form.Item
+            name="lastName"
+            rules={[
+              // { required: true, message: 'Please input your first name!' },
+              {
+                pattern: /^[A-Za-z]+$/,
+                message: 'Please input alphabets only!',
+              },
+            ]}
+          >
+            <Input className="lastNameInput" placeholder="Last Name" />
+          </Form.Item>
+
+          <Form.Item
+            name="email"
+            rules={[
+              // { required: true, message: 'Please input your email!' },
+              {
+                pattern: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/i,
+                message: 'Please enter a valid email address!',
+              },
+            ]}
+          >
+            <Input className="emailInput" placeholder="Email Address" />
+          </Form.Item>
+          <Form.Item
+            name="profile"
+            rules={[{ required: true, message: 'Please select a profile!' }]}
+          >
+            <Select className="selectProfile" placeholder="Select Profile">
+              <Option value="admin">Admin</Option>
+              <Option value="client">Client</Option>
+            </Select>
+          </Form.Item>
+        </Form>
       </Modal>
     </>
   );
